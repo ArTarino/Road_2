411f3f5694e1b20f9933f2a9aba101929a40fc12
ArTarino <man.getto@list.ru>
2024-05-24 21:40:02 +0300

    First commit

--- src/block1/strings/stringUpAndLow.java
+++ src/block1/strings/stringUpAndLow.java
@@ -3,10 +3,8 @@ package block1.strings;
 public class stringUpAndLow {
     public static void main(String[] args) {
         String text = "Меня зовут Кирилл";
-        String upText = changingUpperCase(text);
         System.out.println(text);
     }
-
-    public static String changingUpperCase(String str);
+    int x = 5;
 
 }
